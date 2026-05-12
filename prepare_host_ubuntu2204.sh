fd377a4e123ca856ac6b40ed5d4fea2081ffd765
namenice <32704475+namenice@users.noreply.github.com>
2026-05-12 10:45:27 +0700

    Update prepare_host_ubuntu2204.sh

--- prepare_host_ubuntu2204.sh
+++ prepare_host_ubuntu2204.sh
@@ -82,7 +82,6 @@ cat <<EOF | sudo tee /etc/security/limits.d/ceph.conf > /dev/null
 * hard nofile 1048576
 EOF
 
-
 # --- 9. Install Toolling ---
 echo "tools: Installing debugging tools..."
 apt install -y htop iotop iftop net-tools smartmontools
